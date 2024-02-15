6011f676dde0ae17fe8925f2fc5900b49c3e6bbf
JustYO <yoann.eusebi@hotmail.fr>
2024-02-15 14:09:01 +0100

    (rust) rust env configured

---
+++ bin/pico/rust_lang/target/debug/deps/hello_world-35b6c06394ece1d7.d
@@ -0,0 +1,5 @@
+/Users/justyo/Dev/git_projects/ST2024Theremin/bin/pico/rust_lang/target/debug/deps/hello_world-35b6c06394ece1d7: src/main.rs
+
+/Users/justyo/Dev/git_projects/ST2024Theremin/bin/pico/rust_lang/target/debug/deps/hello_world-35b6c06394ece1d7.d: src/main.rs
+
+src/main.rs:
---
+++ bin/pico/rust_lang/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/justyo/Dev/git_projects/ST2024Theremin/bin/pico/rust_lang/target/debug/hello_world: /Users/justyo/Dev/git_projects/ST2024Theremin/bin/pico/rust_lang/src/main.rs
